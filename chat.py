9ee5dddd2d116f8456d110cea4f3a5710c713013
senthalan <senthalank@gmail.com>
2017-04-18 14:37:16 +0530

    case 3 added with testing

--- chat.py
+++ chat.py
@@ -9,8 +9,7 @@ equality_map = {"greater": ">=", "less": "<=", "equal": "="}
 entity_map = {"number": "ram_size", "amount_of_money": "price", "model": "model", "company": "company", "link": "link"}
 
 
-def send():
-    question = "show me the apple phones with more than 6 gb memory"
+def send(question):
     question = urllib.quote(question)
     url = 'https://api.wit.ai/message?v=20170324&q=' + question
     headers = {'Authorization': 'Bearer ' + access_token}
@@ -21,21 +20,20 @@ def send():
         return -1
     else:
         response = json.loads(r.content)
-    response = response['entities']
-    print response
-    generate(response)
+    # print response['entities']
+    return response['entities']
 
 
 def generate(response):
-    query_template = u"SELECT DISTINCT {select} FROM electronics WHERE {expression}"
-    where_template = u"{entity_key} {equality} {entity_value}"
-    where_between_template = u"{entity_key} BETWEEN {entity_value_one} AND {entity_value_two}"
+    query_template = u"SELECT DISTINCT {function}({column_name}) FROM electronics {where_expression} {order_expression}"
     entities_list = {}
     intent = ''
     select = ''
     equality = ''
     max_min = ''
-    where_list = []
+    order_part = ''
+
+    # reading the response
     for key in response:
         if key == 'intent':
             intent = ((response[key])[0])['value']
@@ -46,44 +44,84 @@ def generate(response):
             for entity in entities:
                 values.append(entity['value'])
             entities_list[key] = values
+
+    # spilt the intent and divide into cases
     intent_split = intent.split('_')
+
+    # case columnName_equality
     if len(intent_split) == 2:
+        select = intent_split[0]
+        equality = intent_split[1]
+    elif len(intent_split) == 3:
+        # case Max/Min_columnName_equality
         if (intent_split[0] == "Max") or (intent_split[0] == "Min"):
-            select = intent_split[1].lower()
             max_min = intent_split[0]
+            select = intent_split[1]
+            equality = intent_split[2]
+        # case Model_max_price
         else:
             select = intent_split[0]
-            equality = intent_split[1]
-            keys = entities_list.keys()
-            for key in keys:
-                entity_value = entities_list.pop(key, None)
-                if (key == "number") or (key == "amount_of_money"):
-                    if equality != "between":
-                        where_query = where_template.format(entity_key=entity_map[key], equality=equality_map[equality],
-                                                            entity_value=str(entity_value[0]))
-                        where_list.append(where_query)
-                    else:
-                        where_query = where_between_template.format(entity_key=entity_map[key],
-                                                                    entity_value_one=str(entity_value[0]),
-                                                                    entity_value_two=str(entity_value[1]))
-                        where_list.append(where_query)
-                else:
-                    where_query = where_template.format(entity_key=key, equality=equality_map["equal"],
-                                                        entity_value="\"" + entity_value[0] + "\"")
-                    where_list.append(where_query)
-    else:
-        print "we are working on !!! "
-    print "select : " + select
-    print "equality : " + equality
-    # print "max min : " + max_min
-    final_where = ""
-    for i in range(0,len(where_list)):
-        final_where += where_list[i] + " "
-        if i != len(where_list) -1:
-            final_where += "AND "
-    final_query = query_template.format(select=select, expression=final_where)
+            order = intent_split[1]
+            order_by = intent_split[2]
+            order_part = build_order_template(order, order_by)
+
+    # joining the queries
+    where_part = build_where_template(entities_list, equality)
+    select = select.lower()
+
+    final_query = query_template.format(function=max_min, column_name=select, where_expression=where_part,
+                                        order_expression=order_part)
     print final_query
+    print
+
+
+def build_where_template(entities_list, equality):
+    where_template = u"{entity_key} {equality} {entity_value}"
+    where_between_template = u"{entity_key} BETWEEN {entity_value_one} AND {entity_value_two}"
+    where_list = []
+    keys = entities_list.keys()
+    for key in keys:
+        entity_value = entities_list.pop(key, None)
+        if (key == "number") or (key == "amount_of_money"):
+            if equality != "between":
+                where_query = where_template.format(entity_key=entity_map[key], equality=equality_map[equality],
+                                                    entity_value=str(entity_value[0]))
+                where_list.append(where_query)
+            else:
+                where_query = where_between_template.format(entity_key=entity_map[key],
+                                                            entity_value_one=str(entity_value[0]),
+                                                            entity_value_two=str(entity_value[1]))
+                where_list.append(where_query)
+        else:
+            where_query = where_template.format(entity_key=key, equality=equality_map["equal"],
+                                                entity_value="\"" + entity_value[0] + "\"")
+            where_list.append(where_query)
+    final_where = ""
+    for i in range(0, len(where_list)):
+        if (i == 0):
+            final_where += "WHERE "
+        final_where += where_list[i] + " "
+        if i != len(where_list) - 1:
+            final_where += "AND "
+    return final_where
+
+
+def build_order_template(order, order_by):
+    order_template = u"ORDER BY {order_by} {order} LIMIT 1"
+    if order.lower() == "max":
+        order = "DES"
+    elif order.lower() == "min":
+        order = "ASC"
+    order_part = order_template.format(order_by = order_by, order = order)
+    return order_part
 
 
 if __name__ == "__main__":
-    send()
+    exit = False
+    while not exit:
+        question = raw_input('Question : ')
+        if question.strip() == "":
+            exit = True
+        else:
+            response = send(question.strip())
+            generate(response)
