9f478351b95638d3c1085812578c9750f12ac590
mathuriga <tmathu24@yahoo.com>
2017-04-20 11:15:10 +0530

    add testing tool

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chathurBot
+
+#Chathur Bot is a computer program which act as a conversational agent to simulate intelligent conversation
+#with one or more users in natural language.
--- readTrainingQuestions.py
+++ readTrainingQuestions.py
@@ -3,3 +3,37 @@ def read_training_questions(intent):
     return questions
 
 
+def get_questions():
+    #user enter the path of the file which contains question and answers
+    path = raw_input("Enter file path:")
+    # Open this file.
+    f = open(path, "r")#Eg path : "/home/mathuriga/CSE-ENG/test"
+
+    i = 0
+    ques = []
+    query = []
+
+    # Loop over each line in the file.
+    for line in f.readlines():
+
+        # Strip the line to remove whitespace.
+        line = line.strip()
+        i += 1
+
+        # Display the line.
+        print(line, i)
+
+        if i == 1:
+            ques.append(line)
+        elif i == 2:
+            query.append(line)
+        else:
+            i = 0
+
+    print('list of questions')
+    for j in range(len(ques)):
+        print (ques[j])
+
+    print('list of queries')
+    for j in range(len(query)):
+        print (query[j])
