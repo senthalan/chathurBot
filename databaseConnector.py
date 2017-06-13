bcd220f21f3397413505060b7327b5e8ebd21e53
senthalan <senthalank@gmail.com>
2017-06-13 22:58:51 +0530

    catch block for query processing

--- databaseConnector.py
+++ databaseConnector.py
@@ -5,7 +5,12 @@ cnx = mysql.connector.connect(host="localhost", user="root", passwd="root", db="
 
 def run_query(query):
     cursor = cnx.cursor(buffered=True)
-    cursor.execute(query)
-    answers = cursor.fetchall()
-    cursor.close()
+    try:
+        cursor.execute(query)
+        answers = cursor.fetchall()
+        cursor.close()
+    except mysql.connector.Error as err:
+        answers = ''
+    finally:
+        cursor.close()
     return answers
