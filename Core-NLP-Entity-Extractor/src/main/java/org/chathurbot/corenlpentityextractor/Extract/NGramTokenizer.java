26bb6b77cd88f1fceb5ce9ddad470d32ba7a2869
Abilashini <abilashini21@yahoo.com>
2017-11-07 15:49:56 +0530

    Adding coreNLP Entity extractor with micro services

---
+++ Core-NLP-Entity-Extractor/src/main/java/org/chathurbot/corenlpentityextractor/Extract/NGramTokenizer.java
@@ -0,0 +1,38 @@
+package org.chathurbot.corenlpentityextractor.Extract;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+
+public class NGramTokenizer {
+
+  public String tokenize(String question) {
+
+    Reader reader = new StringReader(question);
+    StandardTokenizer source = new StandardTokenizer(reader);
+    TokenStream tokenStream = new StandardFilter(source);
+    ShingleFilter shingleFilter = new ShingleFilter(tokenStream, 2, 7);
+
+    CharTermAttribute charTermAttribute = shingleFilter.addAttribute(CharTermAttribute.class);
+    StringBuilder stringBuilder = new StringBuilder();
+
+    try {
+      shingleFilter.reset();
+      while (shingleFilter.incrementToken()) {
+        String string = charTermAttribute.toString();
+        stringBuilder.append(string.replace(" ", "_"));
+        stringBuilder.append(" ");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return stringBuilder.toString();
+  }
+}
