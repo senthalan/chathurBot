be708e2c111939f61cd4ab589a821d35864ac264
senthalan <senthalank@gmail.com>
2017-11-16 08:54:31 +0530

    coreNLP added

--- Core-NLP-Entity-Extractor/src/main/java/org/chathurbot/corenlpentityextractor/Extract/Extractor.java
+++ Core-NLP-Entity-Extractor/src/main/java/org/chathurbot/corenlpentityextractor/Extract/Extractor.java
@@ -14,31 +14,46 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import org.chathurbot.corenlpentityextractor.Extract.NGramTokenizer;
-
 public class Extractor {
 
   public Map getEntities(String question){
     Map<String, String> entities = new HashMap<>();
     Properties props1 = new Properties();
     props1.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, entitymentions");
-    props1.put("ner.model", "/home/mathuriga/CSE-ENG/FYP/chathurBotCoreNLP/chathurBot/Core-NLP-Entity-Extractor/resources/ner-model.ser.gz");
+    props1.put("ner.model", "/home/senthalan/project/fyp/new/chathurBot/Core-NLP-Entity-Extractor/resources/ner-single-word-entities.ser.gz");
     StanfordCoreNLP pipelineAll = new StanfordCoreNLP(props1);
 
     Properties props2 = new Properties();
     props2.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, entitymentions");
-    props2.put("ner.model", "/home/mathuriga/CSE-ENG/FYP/chathurBotCoreNLP/chathurBot/Core-NLP-Entity-Extractor/resources/ner-all.ser.gz");
+    props2.put("ner.model", "/home/senthalan/project/fyp/new/chathurBot/Core-NLP-Entity-Extractor/resources/ner-multiple-word-entities.ser.gz");
     StanfordCoreNLP pipelineModel = new StanfordCoreNLP(props2);
 
     NGramTokenizer tokenizer = new NGramTokenizer();
-    String modelTokens = tokenizer.tokenize(question);
+    String wordCombinations = tokenizer.tokenize(question);
 
-    Annotation documentAll = new Annotation(question);
-    pipelineAll.annotate(documentAll);
-    Annotation documentModel = new Annotation(modelTokens);
-    pipelineModel.annotate(documentModel);
+    Annotation singleWordEntities = new Annotation(question);
+    pipelineAll.annotate(singleWordEntities);
+    Annotation multiWordEntities = new Annotation(wordCombinations);
+    pipelineModel.annotate(multiWordEntities);
 
-    List<CoreMap> sentences1 = documentAll.get(CoreAnnotations.MentionsAnnotation.class);
+    Storage storage = new Storage();
+    storage.setStorage();
+
+    List<CoreMap> sentences2 = multiWordEntities.get(CoreAnnotations.MentionsAnnotation.class);
+    for (CoreMap sentence : sentences2) {
+      for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+        if (!token.get(NamedEntityTagAnnotation.class).equals("O")) {
+          if (token.ner().equals("MODEL") && storage.getModels().contains(token.originalText())) {
+            entities.put(token.originalText(), token.ner());
+          }
+          if (token.ner().equals("RANK") && storage.getRank().contains(token.originalText())) {
+            entities.put(token.originalText(), token.ner());
+          }
+        }
+      }
+    }
+
+    List<CoreMap> sentences1 = singleWordEntities.get(CoreAnnotations.MentionsAnnotation.class);
     for (CoreMap sentence : sentences1) {
       for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
         if (!token.get(NamedEntityTagAnnotation.class).equals("O")) {
@@ -47,15 +62,7 @@ public class Extractor {
       }
     }
 
-    List<CoreMap> sentences2 = documentModel.get(CoreAnnotations.MentionsAnnotation.class);
-    for (CoreMap sentence : sentences2) {
-      for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-        if (!token.get(NamedEntityTagAnnotation.class).equals("O")) {
-          entities.put(token.originalText(), token.ner());
-        }
-      }
-    }
+
     return entities;
   }
-
 }
